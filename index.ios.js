3bc277d576678f2a6f4574c1f4748924c34c6414
Dima <dima@dimas-MacBook-Pro.local>
2017-05-01 20:10:43 +0300

    fix

--- index.ios.js
+++ index.ios.js
@@ -35,14 +35,4 @@ export default class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  
-});
-
 AppRegistry.registerComponent('App', () => App);
